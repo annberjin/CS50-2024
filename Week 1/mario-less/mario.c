317ed6b041af0085e2cc9488d6e730f47fedfe2f
Bernardo <bernardoxie@gmail.com>
2024-06-29 20:43:58 -0400

    Update mario.c
    Cleaned up

--- Week 1/mario-less/mario.c
+++ Week 1/mario-less/mario.c
@@ -1,43 +1,40 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int get_height(void);
 void pyramid(int height);
 
-int main(void) 
+int main(void)
 {
-
-	// Ask for height
-	int height = get_height();
-	pyramid(height); 
-
-// Print the thing
-	
+    // Ask for height
+    int height = get_height();
+    pyramid(height);
+    // Print the pyramid
 }
 
-int get_height(void) 
+int get_height(void)
 {
-	int height;
-	do
-	{
-		height = get_int("Height: ");	
-	}
-	while (height < 1);
-	return height;
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1);
+    return height;
 }
 
 void pyramid(int height)
-{ 
-	for (int row = 0; row < height; row++)
-		{
-			for (int space = 0; space < height - row - 1; space++)
-				{
-					printf(" ");
-				}
-			for (int column = 0; column <= row; column++)
-				{
-					printf("#");
-				}
-			printf("\n");
-		}
+{
+    for (int row = 0; row < height; row++)
+    {
+        for (int space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+        for (int column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
