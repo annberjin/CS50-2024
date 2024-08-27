59cea2379174945011bf91dfd8e13aca731fdc6d
Bernardo <bernardoxie@gmail.com>
2024-08-27 18:00:39 -0400

    Create mario.py

---
+++ week6/sentimental-mario-less/mario.py
@@ -0,0 +1,24 @@
+def main():
+    height = get_int("Height: ")
+    print_pyramid(height)
+
+
+def get_int(prompt):
+    while True:
+        try:
+            n = int(input(prompt))
+
+            if not 1 <= n <= 8:
+                raise ValueError
+            return n
+        except ValueError:
+            pass
+
+
+def print_pyramid(height):
+    for i in range(height):
+        print((" " * (height-i-1)) + ("#"*(i+1)))
+
+
+if __name__ == "__main__":
+    main()
