8c7d0a26273175a1fe06f59ace1290833443f2b4
Bernardo <bernardoxie@gmail.com>
2024-08-27 19:05:06 -0400

    Create cash.py

---
+++ week6/sentimental-cash/cash.py
@@ -0,0 +1,35 @@
+from decimal import Decimal
+
+def main():
+    change = get_float("Change: ")
+    print(calculate(change))
+
+
+def get_float(prompt):
+    while True:
+        try:
+            n = Decimal(input(prompt))
+            return n
+        except ValueError:
+            pass
+
+def calculate(change):
+    change *= 100
+
+    quarters = change // 25
+    change -= quarters * 25
+
+    dimes = change // 10
+    change -= dimes * 10
+
+    nickels = change // 5
+    change -= nickels * 5
+
+    pennies = change / 1
+
+    counter = quarters + dimes + nickels + pennies
+    return int(counter)
+
+
+if __name__ == "__main__":
+    main()
